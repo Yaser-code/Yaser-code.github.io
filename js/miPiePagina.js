f05f77650520b750eaa52716d3d24cac6b405a33
Yaser-code <yaserrojasmolina@gmail.com>
2025-06-29 13:12:42 -0500

    Agregado s1

---
+++ js/miPiePagina.js
@@ -0,0 +1,18 @@
+const piePagina = `
+<footer>
+    <div class="copiaFooter">
+        <p>©2025, Yaser Rojas Molina</p>
+    </div>
+    <div class="contactameFooter">
+        <div>
+            <p>Contactame</p>
+        </div>
+        <div class="logosFooterContatos">
+            <a href="https://github.com/Yaser-code"><i class="fa-brands fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/yaser-rojas-molina-8540a6269?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://api.whatsapp.com/send/?phone=51962733310&text&type=phone_number&app_absent=0" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+        </div>
+    </div>
+</footer>
+`;
+document.getElementById("piePagina").innerHTML = piePagina;
