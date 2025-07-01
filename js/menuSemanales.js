4dfa57567a17907aa895c2cd79635f25338aa3b6
Yaser-code <yaserrojasmolina@gmail.com>
2025-06-30 19:04:31 -0500

    Agregando cabecera en las semanas 1,2

---
+++ js/menuSemanales.js
@@ -0,0 +1,18 @@
+const menuSemanal  = (
+    `<div class="cabecera-semana">
+        <div class="inicioSemana">
+            <img src="../img/YaserLogo.png" alt="Logo">
+            <a href="../index.html"><i class="fa-solid fa-house"></i> Página Principal</a>
+        </div>
+        <nav class="menuSemana">
+            <a href="../pages/semana01.html" class="menuItem"><span class="material-symbols-outlined">book_2</span> Semana 01</a>
+            <a href="../pages/semana02.html" class="menuItem"><span class="material-symbols-outlined">book_2</span> Semana 02</a>
+            <a href="../pages/semana03.html" class="menuItem"><span class="material-symbols-outlined">book_2</span> Semana 03</a>
+            <a href="#" class="menuItem"><span class="material-symbols-outlined">book_2</span> Semana 04</a>
+            <a href="#" class="menuItem"><span class="material-symbols-outlined">book_2</span> Semana 05</a>
+            <a href="#" class="menuItem"><span class="material-symbols-outlined">book_2</span> Semana 06</a>
+            <a href="#" class="menuItem"><span class="material-symbols-outlined">book_2</span> Semana 07</a>
+        </nav>
+    </div>`
+);
+document.getElementById("cabeceraGeneral").innerHTML = menuSemanal;
